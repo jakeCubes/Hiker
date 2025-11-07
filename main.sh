caad445682f5944c21fdbbee4bccefb234837387
jakeCubes <cubesgoround@proton.me>
2025-11-07 21:15:43 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -92,7 +92,8 @@ clear
 
 #Installing the Hiker Desktop------------------------------------------------------
 sleep 0.5
-apk add xterm xorg-server xinit xf86-input-libinput icewm nitrogen xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-kde xdg-desktop-portal-xapp jgmenu
+apk add jgmenu
+apk add xterm xorg-server xinit xf86-input-libinput icewm nitrogen xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-kde xdg-desktop-portal-xapp
 
 apk add greetd greetd-openrc greetd-tuigreet
 while [ ! -f "/etc/init.d/greetd" ]; do
